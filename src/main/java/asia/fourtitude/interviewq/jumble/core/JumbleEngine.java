c26378fcd7582e57fc23878ffcae2031c2cadec0
cqin626 <qinhoh@gmail.com>
2026-01-22 11:25:12 +0800

    Task A: Implement scramble method

--- src/main/java/asia/fourtitude/interviewq/jumble/core/JumbleEngine.java
+++ src/main/java/asia/fourtitude/interviewq/jumble/core/JumbleEngine.java
@@ -5,6 +5,8 @@ import java.util.*;
 
 public class JumbleEngine {
 
+    private static final Random RANDOM = new Random();
+
     /**
      * From the input `word`, produces/generates a copy which has the same
      * letters, but in different ordering.
@@ -15,15 +17,32 @@ public class JumbleEngine {
      * a) pass unit test: JumbleEngineTest#scramble()
      * b) scrambled letters/output must not be the same as input
      *
-     * @param word  The input word to scramble the letters.
-     * @return  The scrambled output/letters.
+     * @param word The input word to scramble the letters.
+     * @return The scrambled output/letters.
      */
     public String scramble(String word) {
-        /*
-         * Refer to the method's Javadoc (above) and implement accordingly.
-         * Must pass the corresponding unit tests.
-         */
-        throw new UnsupportedOperationException("to be implemented");
+        if (word == null || word.length() < 2) {
+            return word;
+        }
+
+        if (word.chars().distinct().count() == 1) {
+            return word;
+        }
+
+        char[] charArr = word.toCharArray();
+        String scrambledWord;
+
+        do {
+            for (int i = charArr.length - 1; i > 0; i--) {
+                int randInt = RANDOM.nextInt(i + 1);
+                char temp = charArr[i];
+                charArr[i] = charArr[randInt];
+                charArr[randInt] = temp;
+            }
+            scrambledWord = String.valueOf(charArr);
+        } while (scrambledWord.equals(word));
+
+        return scrambledWord;
     }
 
     /**
@@ -40,7 +59,7 @@ public class JumbleEngine {
      * c) using "try-with-resources" functionality/statement
      * d) pass unit test: JumbleEngineTest#palindrome()
      *
-     * @return  The list of palindrome words found in system/engine.
+     * @return The list of palindrome words found in system/engine.
      * @see https://www.google.com/search?q=palindrome+meaning
      */
     public Collection<String> retrievePalindromeWords() {
@@ -59,10 +78,10 @@ public class JumbleEngine {
      * b) provide a good enough implementation, if not able to provide a fast lookup
      * c) bonus points, if able to implement a fast lookup/scheme
      *
-     * @param length  The word picked, must of length.
-     *                When length is null, then return random word of any length.
-     * @return  One of the word (randomly) from word list.
-     *          Or null if none matching.
+     * @param length The word picked, must of length.
+     *               When length is null, then return random word of any length.
+     * @return One of the word (randomly) from word list.
+     *         Or null if none matching.
      */
     public String pickOneRandomWord(Integer length) {
         /*
@@ -81,8 +100,8 @@ public class JumbleEngine {
      * b) provide a good enough implementation, if not able to provide a fast lookup
      * c) bonus points, if able to implement a fast lookup/scheme
      *
-     * @param word  The input word to check.
-     * @return  true if `word` exists in internal word list.
+     * @param word The input word to check.
+     * @return true if `word` exists in internal word list.
      */
     public boolean exists(String word) {
         /*
@@ -105,8 +124,8 @@ public class JumbleEngine {
      * b) provide a good enough implementation, if not able to provide a fast lookup
      * c) bonus points, if able to implement a fast lookup/scheme
      *
-     * @param prefix  The prefix to match.
-     * @return  The list of words matching the prefix.
+     * @param prefix The prefix to match.
+     * @return The list of words matching the prefix.
      */
     public Collection<String> wordsMatchingPrefix(String prefix) {
         /*
@@ -135,10 +154,10 @@ public class JumbleEngine {
      * b) provide a good enough implementation, if not able to provide a fast lookup
      * c) bonus points, if able to implement a fast lookup/scheme
      *
-     * @param startChar  The first character of the word to search for.
-     * @param endChar    The last character of the word to match with.
-     * @param length     The length of the word to match.
-     * @return  The list of words matching the searching criteria.
+     * @param startChar The first character of the word to search for.
+     * @param endChar   The last character of the word to match with.
+     * @param length    The length of the word to match.
+     * @return The list of words matching the searching criteria.
      */
     public Collection<String> searchWords(Character startChar, Character endChar, Integer length) {
         /*
@@ -160,18 +179,18 @@ public class JumbleEngine {
      * The sub words must exist in internal word list.
      *
      * Example: From "yellow" and `minLength` = 3, the output sub words:
-     *     low, lowly, lye, ole, owe, owl, well, welly, woe, yell, yeow, yew, yowl
+     * low, lowly, lye, ole, owe, owl, well, welly, woe, yell, yeow, yew, yowl
      *
      * Evaluation/Grading:
      * a) pass related unit tests in "JumbleEngineTest"
      * b) provide a good enough implementation, if not able to provide a fast lookup
      * c) bonus points, if able to implement a fast lookup/scheme
      *
-     * @param word       The input word to use as base/seed.
-     * @param minLength  The minimum length (inclusive) of sub words.
-     *                   When zero, return empty list.
-     *                   Default is 3.
-     * @return  The list of sub words constructed from input `word`.
+     * @param word      The input word to use as base/seed.
+     * @param minLength The minimum length (inclusive) of sub words.
+     *                  When zero, return empty list.
+     *                  Default is 3.
+     * @return The list of sub words constructed from input `word`.
      */
     public Collection<String> generateSubWords(String word, Integer minLength) {
         /*
@@ -188,12 +207,12 @@ public class JumbleEngine {
      * Word is of length 6 characters.
      * The minimum length of sub words is of length 3 characters.
      *
-     * @param length     The length of selected word.
-     *                   Expects >= 3.
-     * @param minLength  The minimum length (inclusive) of sub words.
-     *                   Expects positive integer.
-     *                   Default is 3.
-     * @return  The game state.
+     * @param length    The length of selected word.
+     *                  Expects >= 3.
+     * @param minLength The minimum length (inclusive) of sub words.
+     *                  Expects positive integer.
+     *                  Default is 3.
+     * @return The game state.
      */
     public GameState createGameState(Integer length, Integer minLength) {
         Objects.requireNonNull(length, "length must not be null");
@@ -206,7 +225,8 @@ public class JumbleEngine {
             throw new IllegalArgumentException("Invalid length=[" + length + "], expect greater than or equals 3");
         }
         if (minLength > length) {
-            throw new IllegalArgumentException("Expect minLength=[" + minLength + "] greater than length=[" + length + "]");
+            throw new IllegalArgumentException(
+                    "Expect minLength=[" + minLength + "] greater than length=[" + length + "]");
         }
         String original = this.pickOneRandomWord(length);
         if (original == null) {
